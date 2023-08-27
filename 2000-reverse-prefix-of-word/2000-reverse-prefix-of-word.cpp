9ad6affadc09041d5042712d5bebca4fb5d3ce77
Muhammad Rafay <86618279+MUHAMMAD-ABDUL-RAFAY@users.noreply.github.com>
2023-08-27 22:01:03 +0500

    Time: 0 ms (100.00%), Space: 6.1 MB (66.85%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        stack<char> st;
         int pos = -1;
         for(int i=0;i<word.length();i++){
-            st.push(word[i]);
             if(ch == word[i]){
                 pos = i;
                 break;
