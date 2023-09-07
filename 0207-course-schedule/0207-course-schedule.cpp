b1027a4386c5daacdfafba479a01ca886c3c63d1
Muhammad Rafay <86618279+MUHAMMAD-ABDUL-RAFAY@users.noreply.github.com>
2023-09-07 11:42:51 +0500

    Time: 137 ms (5.15%), Space: 82.8 MB (5.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses, vector<int>(numCourses));
-        vector<bool> visited(numCourses,false);
         vector<int> indegree(numCourses,0);
         queue<int> q;
         for(int i=0;i<numCourses;i++){
