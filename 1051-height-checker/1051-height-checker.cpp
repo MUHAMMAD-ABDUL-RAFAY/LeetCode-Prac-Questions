4c28e14f5fb11f799ccbb9f187d697f546ab59fe
Muhammad Rafay <86618279+MUHAMMAD-ABDUL-RAFAY@users.noreply.github.com>
2024-06-10 13:12:38 +0500

    Time: 0 ms (100.00%), Space: 10.1 MB (44.04%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> compare(heights);
-        sort(compare.begin(),compare.end());
-        int count = 0;
+        vector<int> checker;
+        checker = heights;
+        sort(checker.begin(),checker.end());
+        int ans = 0;
         for(int i=0;i<heights.size();i++){
-            if(heights[i] != compare[i])
-                count++;
+            if(heights[i] != checker[i])
+                ans++;
         }
-        return count;
-        
+        return ans;
     }
 };
